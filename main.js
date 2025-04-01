54e952be24339aae0fbfe4b1aa4bd1b2a46a9f9b
viatransporte01 <viatransporte01@gmail.com>
2025-04-01 11:58:38 +0000

    Função para calcular a gorjeta e o total por pessoa finalizada

--- main.js
+++ main.js
@@ -5,28 +5,30 @@ let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.querySelector("#bill").valueAsNumber
+    calculate()
 }
 
 function receiveNumberOfPeopleValue() {
     numberOfPeople = document.querySelector("#people").valueAsNumber
+    calculate()
 }
 
-function receiveTipPercentagevalue(value) {
+function receiveTipPercentageValue(value) { 
     if (buttonSelected !== null) {
         buttonSelected.classList.remove("button-selected")
     }
 
     let customTipInput = document.querySelector("#custom-tip")
 
-
     if(customTipInput.value !== "") {
         customTipInput.value = ""
     }
 
-  tipPercentage = value / 100
+    tipPercentage = value / 100
 
-  buttonSelected = document.querySelector(`#button-${value}`)
-  buttonSelected.classList.add("button-selected")
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
+    calculate()
 }
 
 function receiveCustomTipPercentageValue(){
@@ -35,5 +37,20 @@ function receiveCustomTipPercentageValue(){
         buttonSelected = null
     }
 
-    tipPercentage = document.querySelector("#custom-tip").valueAsBumber / 100
+    tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100 
+    calculate()
+}
+
+function calculate() {
+    if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0) {
+        let amountStrong = document.querySelector(".amount strong")
+        let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+        amountStrong.innerText = `$${tipAmountPerson.toFixed(2)}`
+        
+        let totalStrong = document.querySelector(".total strong")
+        let totalPerson = (bill / numberOfPeople) + tipAmountPerson
+        totalStrong.innerText = `$${totalPerson.toFixed(2)}`
+    } else {
+        console.log("Preencha tudo")
+    }
 }
